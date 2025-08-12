e1077ef5d872743ce75d215d55e1da6d9ac70929
Natchanan001 <alwaysforever1112@gmail.com>
2025-08-12 19:36:35 +0700

    4.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'sp': '0 3px 12px 0 rgba(0, 0, 0, 0.2)',
+      },
+    },
   },
   plugins: [],
 }
