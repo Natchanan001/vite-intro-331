8418bf40c6e5033730a208e293fb4508a1be83b3
Natchanan001 <alwaysforever1112@gmail.com>
2025-08-14 02:58:36 +0700

    8-9

--- vue-lab02/src/App.vue
+++ vue-lab02/src/App.vue
@@ -13,10 +13,10 @@ const { messages } = storeToRefs(messageStore);
       <h4>{{ messages }}</h4>
     </div>
      <div class="wrapper">
-       <nav>
-         <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink> |
-         <RouterLink :to="{ name: 'students' }">Student</RouterLink> |
-         <RouterLink :to="{ name: 'about' }">About</RouterLink>
+       <nav class="py-6">
+         <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'event-list-view' }">Event</RouterLink> |
+         <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'students' }">Student</RouterLink> |
+         <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'about' }">About</RouterLink>
        </nav>
      </div>
    </header>
